6800ad72c401720efc8916b7956bfe0e5ddad6a2
bogachev-pa <bogachev.pa@gmail.com>
2015-09-19 20:56:48 +0300

    RAO simulation language: add 'Alignment' keyword

--- src/languages/rao.js
+++ src/languages/rao.js
@@ -14,7 +14,7 @@ function(hljs) {
         'frame background if else for text rectangle line circle ellipse triangle color ' +
         'enum int double boolean ' +
         'String array break return resource or ' +
-        'and mod not exist notExist forAll except empty size left center right',
+        'and mod not exist notExist forAll except empty size left center right Alignment',
       constant: 'true false',
       built_in:'select first withMin withMax setValueBefore setValueAfter ' +
         'setParent setCondition setPriority setTerminateCondition evaluateBy ' +
