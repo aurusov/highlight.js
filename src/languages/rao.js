cdc74bfd1ce0a99d7f3b3d909d764152ef4bee3c
Andrey Urusov <drobus@gmail.com>
2016-09-25 16:17:14 +0300

    Новый синтаксис на xbase (aurusov/raox#373)

--- src/languages/rao.js
+++ src/languages/rao.js
@@ -7,27 +7,27 @@ function(hljs) {
     case_insensitive: true,
     lexemes: /[a-zA-Z_$][a-zA-Z0-9_]*/,
     keywords: {
-      keyword: 'type relevantSet operation keyboard rule relevant event any ' +
-        'dpt set activity checks search ' +
-        'sequence enumerative legacy uniform exponential normal triangular histogram constant ' +
-        'function algorithmic table list result ' +
-        'frame background if else for text rectangle line circle ellipse triangle Color ' +
-        'enum int double boolean ' +
-        'String array break return resource or ' +
-        'and mod not exist notExist forAll except empty size left center right Alignment ' +
-        'BLACK BLUE CYAN DARK_BLUE DARK_CYAN DARK_GRAY DARK_GREEN DARK_MAGENTA DARK_RED ' +
-        'DARK_YELLOW GRAY GREEN MAGENTA RED WHITE YELLOW',
+      keyword: 'type new operation rule relevant event ' +
+        'logic edge set activity search sequence constant ' +
+        'result resultType var if else for ' +
+        'enum int double boolean Value ' +
+        'String array break return resource ' +
+        'frame background drawText drawRectangle drawLine drawCircle drawEllipse drawTriangle ' +
+        'RaoColor BLACK BLUE CYAN DARK_BLUE DARK_CYAN DARK_GRAY DARK_GREEN DARK_MAGENTA DARK_RED ' +
+        'DARK_YELLOW GRAY GREEN MAGENTA RED WHITE YELLOW ' +
+        'Alignment LEFT CENTER RIGHT',
       constant: 'true false',
-      built_in:'select first withMin withMax setValueBefore setValueAfter ' +
-        'setParent setCondition setPriority setTerminateCondition evaluateBy ' +
-        'compareTops terminateCondition init watchParameter watchState watchQuantity ' +
-        'watchValue getValue duration begin end execute stop plan ' +
+      built_in:'accessible all filter any onlyif exists forall next minBySafe maxBySafe ' +
+        'startCondition heuristic compareTops terminateCondition init draw ' +
+        'DiscreteHistogram Uniform Exponential Normal Triangular ' +
+        'duration begin end execute stop plan ' +
         'create erase currentTime'
     },
     contains: [
       hljs.C_LINE_COMMENT_MODE,
       hljs.C_BLOCK_COMMENT_MODE,
       hljs.QUOTE_STRING_MODE,
+      hljs.APOS_STRING_MODE,
       hljs.C_NUMBER_MODE
     ]
   };
